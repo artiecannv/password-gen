137f4d12c7d498faed555791dab26e2283cc1f10
artiecannv <thefiftharthur@gmail.com>
2022-10-27 22:33:00 -0600

    added acceptance criteria and started working on my function

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 // Assignment code here
 
+/*
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+*/
+
+//Will need to use prompt, confirm, and alert method
+//Password needs to be between 8 and characters
+//User needs the ability to choose 1-4 types of characters
+//Needs number generator
+//Needs to validate that their criteria has been met at least once
+
+function generatePassword() {
+  let length = prompt("Password length can be between 8 and 128 characters", 8);
+  let low = confirm("Include Lowercase Characters?")
+  let upper = confirm("Include Uppercase Characters?")
+  let numeric = confirm("Include Numeric Characters? (0-9)")
+  let special = confirm("Include Special Characters? (!<>@?)")
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +47,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
