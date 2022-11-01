cb282868a1de2ac1247d774805364e43f0da7117
artiecannv <thefiftharthur@gmail.com>
2022-11-01 17:44:42 -0600

    all but validation criteria is working

--- script.js
+++ script.js
@@ -18,12 +18,6 @@ WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
 */
 
-
-
-
-//Needs number generator
-//Needs to validate that their criteria has been met at least once
-
 function generatePassword() {
   //Logs to the console, to show the button is working
   console.log("Button Test!")
@@ -34,89 +28,154 @@ function generatePassword() {
   let upper = false;
   let numeric = false;
   let special = false;
-  
+
 
   //Storage of all possible characters
   let validCrit = [];
   const lowChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   const upChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   const numChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-  const specChar = [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", ":", "|", "}", "~" ];
+  const specChar = [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", ":", "|", "}", "~"];
   let genPass = "";
 
-// Validate user prompts for Acceptable Criteria
-do {
+  //Storage and Access of Chosen Characters
+  function createPassword() {
+    genPass = "";
+    for (let index = 0; index < length; index++) {
 
-  length = prompt("Password length can be between 8 and 128 characters", 8);
-  length = Number(length);
- /* console.log(length);
-  console.log(typeof(length)); */
+      // Create way to generate random number to reference valid characters
+      let x = Math.floor(Math.random() * validCrit.length);
+
+      genPass += validCrit[x];
+
+      console.log(genPass);
+    }
+  }
+
+  // Validate user prompts for Acceptable Criteria
+  do {
+
+    length = prompt("Password length can be between 8 and 128 characters", 8);
+    length = Number(length);
+    /* console.log(length);
+     console.log(typeof(length)); */
+
+  } while (length < 8 || length > 128 || isNaN(length));
+
+  let charCrit = false;
+
+  /* Checking to see if at least one of the character criteria options are being selected
+   and adding to the validCrit Array */
+  while (!charCrit) {
+
+    if (!charCrit) {
+      alert("Make sure that the OK button is selected for at least one of the following options.");
+    }
+
+    low = confirm("Include Lowercase Characters?");
+    upper = confirm("Include Uppercase Characters?");
+    numeric = confirm("Include Numeric Characters? (0-9)");
+    special = confirm("Include Special Characters? (!<>@?)");
+    console.log('testing loop');
+
+    if (low) {
+      charCrit = true;
+      validCrit = validCrit.concat(lowChar);
+      console.log(validCrit);
+    }
+
+    if (upper) {
+      charCrit = true;
+      validCrit = validCrit.concat(upChar);
+      console.log(validCrit);
+    }
+
+    if (numeric) {
+      charCrit = true;
+      validCrit = validCrit.concat(numChar);
+      console.log(validCrit);
+    }
+
+    if (special) {
+      charCrit = true;
+      validCrit = validCrit.concat(specChar);
+      console.log(validCrit);
+    }
+
+  }
+
+  createPassword();
+
+  // Validating that password hits all criteria selected
+  // Use Includes Method
+
+  let validLow = false
+  let validUpper = false
+  let validNum = false
+  let validSpec = false
+
+  for (let index = 0; index < genPass.length; index++) {
+    console.log(genPass[index]);
+
+    if (low) {
+
+      validLow = lowChar.includes(genPass[index]);
+
+    }
+
+    if (upper) {
+
+      validUpper = upChar.includes(genPass[index]);
+
+    }
+
+    if (numeric) {
+
+      validNum = numChar.includes(genPass[index]);
+
+    }
+
+    if (special) {
+
+      validSpec = specChar.includes(genPass[index]);
+
+    }
+
+  }
+/*
+  let rerunCrit = false;
+
+  let counter = 0
+
+  do {
+
+    counter++
+
+    if (low && !validLow) {
+      createPassword();
+    } else if (upper && !validUpper) {
+      createPassword();
+    } else if (numeric && !validNum) {
+      createPassword();
+    } else if (special && validSpec) {
+      createPassword();
+    } else {
+      break;
+      rerunCrit = true;
   
-} while (length < 8 || length > 128 || isNaN(length));
+    }
 
-let charCrit = false;
+    console.log(counter)
 
-/* Checking to see if at least one of the character criteria options are being selected
- and adding to the validCrit Array */ 
-while(!charCrit) {
-
-  if (!charCrit) {
-    alert("Make sure that the OK button is selected for at least one of the following options.");
-  }
-
-  low = confirm("Include Lowercase Characters?");
-  upper = confirm("Include Uppercase Characters?");
-  numeric = confirm("Include Numeric Characters? (0-9)");
-  special = confirm("Include Special Characters? (!<>@?)");
-  console.log('testing loop');
-
-  if (low) {
-    charCrit = true;
-    validCrit = validCrit.concat(lowChar);
-    console.log(validCrit);
-  }
-
-  if (upper) {
-    charCrit = true;
-    validCrit = validCrit.concat(upChar);
-    console.log(validCrit);
-  }
-
-  if (numeric) {
-    charCrit = true;
-    validCrit = validCrit.concat(numChar);
-    console.log(validCrit);
-  }
-
-  if (special) {
-    charCrit = true;
-    validCrit = validCrit.concat(specChar);
-    console.log(validCrit);
-  }
-
-}
-
-//Storage and Access of Chosen Characters
-for (let index = 0; index < length; index++) {
-
-  // Create way to generate random number to reference valid characters
-  let x = Math.floor(Math.random() * validCrit.length);
-
-  genPass += validCrit[x]; 
-
-  console.log(genPass);
-}
-
-/* TO DO - store references to randomly selected valid characters
-  AND/OR store randomly selected characters
+  } while (!rerunCrit || counter < 10);
 */
+  
+
+   
 
 
-// TO DO - if not already, turn password into a string
-// TO DO - save new password to variable
-
-//Return the validated new password
-return genPass;
+  //Return the validated new password
+  return genPass;
 }
 
 // Get references to the #generate element
